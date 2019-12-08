fa7d2b14301320d7966227787aa1d057c2da4104
Nazmul Alom <nazmulshuvo03@gmail.com>
2019-12-08 23:44:15 +0600

    front end and back end connected

--- src/components/create-user.js
+++ src/components/create-user.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class CreateUsers extends Component {
 	constructor(props) {
@@ -23,6 +24,8 @@ class CreateUsers extends Component {
 		};
 
 		console.log(user);
+
+		axios.post('http://localhost:5000/users/add', user).then((res) => console.log(res.data));
 
 		this.setState({
 			username: ''
